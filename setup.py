2e5ea20b2274a411856e21ae502ef50b1eb50961
Lasse Meixner <lassemeixner@gmail.com>
2022-10-16 17:49:41 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nof1simulation',
+      version='0.1',
+      description='Small package for generating Nof1 experimental data and performing some statistical tests',
+      url='https://github.com/lasse-meixner/Nof1-Simulation-Studies',
+      author='Lasse von der Heydt',
+      author_email='lassemeixner@gmail.com',
+      license='MIT',
+      packages=['nof1simulation'],
+      zip_safe=False)
