1aabd96f1d229a23ad4f71cf5bd8a5098dbd3a64
Lasse Meixner <lassemeixner@gmail.com>
2022-10-19 21:17:25 +0100

    correciton

--- nof1simulation/ar1_model.r
+++ nof1simulation/ar1_model.r
@@ -4,8 +4,8 @@ library(nlme)
 ar1_model = function(
   y0,y1,sub,T,t,c0
 ){
-  m0 <- nlme::lme(y0 ~ T + t + c0, random = ~ 1 | ID, correlation = corAR1( form = ~ 1 | ID))
-  m1 <- nlme::lme(y1 ~ T + t + c0, random = ~ 1 | ID, correlation = corAR1( form = ~ 1 | ID))
+  m0 <- nlme::lme(y0 ~ T + t + c0, random = ~ 1 | sub, correlation = corAR1( form = ~ 1 | sub))
+  m1 <- nlme::lme(y1 ~ T + t + c0, random = ~ 1 | sub, correlation = corAR1( form = ~ 1 | sub))
   p0_value <- summary(fit)$tTable[2,5]
   t1_value <- summary(fit)$tTable[2,4]
   fe1_value <- summary(fit)$tTable[2,1]
