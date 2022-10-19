aefbadd23346b78f505acbe4f8cb74abda0fd96d
Lasse Meixner <lassemeixner@gmail.com>
2022-10-19 21:42:59 +0100

    add r function as str

--- nof1simulation/CrossOverExperiment.py
+++ nof1simulation/CrossOverExperiment.py
@@ -1,9 +1,27 @@
 import numpy as np
-from scipy.stats import ttest_ind, ttest_1samp, t, norm
+from scipy.stats import ttest_1samp, t
 from tqdm import tqdm
 from statsmodels.api import MixedLM
 
-# Objects
+# R Objects
+ar1_model_r = """
+library(nlme)
+
+ar1_model = function(
+  y0,y1,sub,T,t,c0
+){
+  y0 <- as.vector(y0)
+  y1 <- as.vector(y1)
+  sub <- as.vector(sub)
+  t <- as.vector(t)
+  c0 <- as.vector(c0)
+  m0 <- nlme::lme(y0 ~ T + t + c0, random = ~ 1 | sub, correlation = corAR1( form = ~ 1 | sub))
+  m1 <- nlme::lme(y1 ~ T + t + c0, random = ~ 1 | sub, correlation = corAR1( form = ~ 1 | sub))
+  p0_value <- summary(m0)$tTable[2,5]
+  t1_value <- summary(m1)$tTable[2,4]
+  fe1_value <- summary(m1)$tTable[2,1]
+  return(c(p0_value,t1_value,fe1_value))
+}"""
 
 class CrossOverExperiment():
     #
