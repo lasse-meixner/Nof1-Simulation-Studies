fd8c141c7d67fec08879670d79d3643344f6eb5b
Lasse Meixner <lassemeixner@gmail.com>
2022-10-19 21:38:02 +0100

    attempting non-comfotable array fix

--- nof1simulation/ar1_model.r
+++ nof1simulation/ar1_model.r
@@ -4,6 +4,11 @@ library(nlme)
 ar1_model = function(
   y0,y1,sub,T,t,c0
 ){
+  y0 <- as.vector(y0)
+  y1 <- as.vector(y1)
+  sub <- as.vector(sub)
+  t <- as.vector(t)
+  c0 <- as.vector(c0)
   m0 <- nlme::lme(y0 ~ T + t + c0, random = ~ 1 | sub, correlation = corAR1( form = ~ 1 | sub))
   m1 <- nlme::lme(y1 ~ T + t + c0, random = ~ 1 | sub, correlation = corAR1( form = ~ 1 | sub))
   p0_value <- summary(fit)$tTable[2,5]
