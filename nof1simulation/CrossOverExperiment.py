804634fb0649043c19f523a8ac7f77bfaa4e7455
Lasse Meixner <lassemeixner@gmail.com>
2022-10-21 15:02:06 +0100

    fixed e.message

--- nof1simulation/CrossOverExperiment.py
+++ nof1simulation/CrossOverExperiment.py
@@ -244,7 +244,7 @@ class CrossOverExperiment():
                 try:
                     p0,t0,t1,fe1 = ar1_model.simple_model(y0,y1,sub,T,t,cO)
                 except Exception as e:
-                    print(f"Exception in iteration {i} for {self.params}.",e.message)
+                    print(f"Exception in iteration {i} for {self.params}.",e)
                     continue
                 self.p_values.append(p0)
                 self.null_statistics.append(t0)
